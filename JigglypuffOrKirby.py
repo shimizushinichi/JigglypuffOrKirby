a3e184f3b1bd837c83a9aee29527269839550c65
shimizushinichi <23429755+shimizushinichi@users.noreply.github.com>
2020-03-17 14:41:35 +0900

    run_augmenter関数を追加

--- JigglypuffOrKirby.py
+++ JigglypuffOrKirby.py
@@ -10,8 +10,12 @@ def user_input():
     parser.add_argument("-s", "--site", help="website to crawl", type=str, default = "flickr", choices=["flickr"])
     parser.add_argument("-ch", "--character", help="Search word", type=str)
     parser.add_argument("-im", "--images_folder_path", help="The path of image's folder", type=str)
-    # parser.add_argument("", help="")
-    # parser.add_argument("", help="")
+    parser.add_argument("-in", "--input", help="input folder path", type=str)
+    parser.add_argument("-out", "--output", help="output folder path", type=str, default = "augmented")
+    # parser.add_argument("", help="", type=str)
+    # parser.add_argument("", help="", type=str)
+    # parser.add_argument("", help="", type=str)
+
     args_namespace = parser.parse_args()
     arguments = vars(args_namespace)
     return arguments
@@ -23,11 +27,18 @@ def run_crawler(site, character):
     crawler.get_images(character)
 
 def run_trimmer(images_folder_path, character):
-    assert os.path.isdir, "-imに指定されたディレクトリが見つかりません。input:{}".format(images_folder_path)
+    assert os.path.isdir(images_folder_path), "-imに指定されたディレクトリが見つかりません。input:{}".format(images_folder_path)
     assert character, "-chが空白か無効な値になっています。input:{}".format(character)
     import_file = "trimming.trimmer"
     trimmer_module = importlib.import_module(import_file)
     trimmer_module.trimmer(images_folder_path, character)
+
+def run_augmenter(input_dir, output_dir, character):
+    assert os.path.isdir(input_dir), "-inに指定されたディレクトリが見つかりません。input:{}".format(input_dir)
+    assert character, "-chが空白か無効な値になっています。input:{}".format(character)
+    import_file = "augmentation.augmenter"
+    augmenter_module = importlib.import_module(import_file)
+    augmenter_module.augmenter(input_dir, output_dir, character)
 
 def main():
     arguments = user_input()
@@ -37,7 +48,8 @@ def main():
     elif arguments["func"] == "trimming":
         run_trimmer(arguments["images_folder_path"], arguments["character"])
 
-    # elif arguments["func"] == augmentation:
+    elif arguments["func"] == "augmentation":
+        run_augmenter(arguments["input"], arguments["output"], arguments["character"])
     # elif arguments["func"] == learn:
     # elif arguments["func"] == judge:
 
