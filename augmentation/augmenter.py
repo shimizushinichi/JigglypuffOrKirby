cc5fbef5bbd9b2fd1ecdb952d843b9eb8c804e32
shimizushinichi <23429755+shimizushinichi@users.noreply.github.com>
2020-03-25 13:46:11 +0900

    augmenter.pyの中身を関数に分ける

--- augmentation/augmenter.py
+++ augmentation/augmenter.py
@@ -1,18 +1,7 @@
 import os
 from PIL import Image
 
-def augmenter(input_dir):
-    if input_dir[-1] == "/":
-        input_dir = input_dir[:-1]
-    character = os.path.basename(input_dir)
-
-    output_dir = os.path.join("augmented", character)
-    try:
-        os.makedirs(output_dir)
-    except FileExistsError:
-        pass
-
-    file_list = os.listdir(input_dir)
+def rotate_and_save_img(file_list, input_dir, output_dir):
     for file_name in file_list:
         basename, ext = os.path.splitext(file_name)
         if ext in {".png", ".jpg", ".jpeg"}:
@@ -26,7 +15,12 @@ def augmenter(input_dir):
                 rotate_num = "_" + str((i+1) * 90)
                 tmp_img.save(output_dir + "/" + basename + rotate_num + ext)
 
-            #画像の大きさを取得する
+def shrink_rotate_and_save_img(file_list, input_dir, output_dir):
+    for file_name in file_list:
+        basename, ext = os.path.splitext(file_name)
+        if ext in {".png", ".jpg", ".jpeg"}:
+            img = Image.open(input_dir + "/" + file_name)
+
             width, height = img.size
             #画像を半分に縮小する
             img.thumbnail((width//2, height//2), Image.LANCZOS)
@@ -42,3 +36,18 @@ def augmenter(input_dir):
                 shrinked_img = shrinked_img.transpose(Image.ROTATE_90)
                 rotate_num = "_" + str((i+1) * 90)
                 shrinked_img.save(output_dir + "/" + basename + "_shrinked" + rotate_num + ext)
+
+def augmenter(input_dir):
+    if input_dir[-1] == "/":
+        input_dir = input_dir[:-1]
+    character = os.path.basename(input_dir)
+
+    output_dir = os.path.join("augmented", character)
+    try:
+        os.makedirs(output_dir)
+    except FileExistsError:
+        pass
+
+    file_list = os.listdir(input_dir)
+    rotate_and_save_img(file_list, input_dir, output_dir)
+    shrink_rotate_and_save_img(file_list, input_dir, output_dir)
